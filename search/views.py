5d5a14dabc3e1f23497a6a956cbf58787dd3454a
Danil Mironov <danmir@me.com>
2013-07-28 12:09:50 +0600

    Search by keyword

--- search/views.py
+++ search/views.py
@@ -8,10 +8,15 @@ from django.contrib.flatpages.models import FlatPage
 def search(request):
     query = request.GET.get('q', '')
     results = []
+    keyword_results = []
     if query:
+        #the keyword field on the related SearchKeyword model
+        keyword_results = FlatPage.objects.filter(searchkeyord__keyword__in = query.split()).distinct()
         results = FlatPage.objects.filter(content__icontains=query)
     #template = get_template('search/search.html')
     #context = Context({'query': query, 'results': results})
     #response = template.render(context)
     #return HttpResponse(response)
-    return render_to_response('search/search.html', {'query': query, 'results': results})
+    return render_to_response('search/search.html', {'query': query,
+                                                     'results': results,
+                                                     'keyword_results': keyword_results})
